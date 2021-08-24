6600c4b0dd75506a646cd15193f7203c7d9bfb29
zhuhc <1647358411@qq.com>
2021-08-24 15:38:22 +0800

    add download tools class

---
+++ OCToolsClass/OCToolsClass/Tools/Download/HMDownloadManager.h
@@ -0,0 +1,16 @@
+//
+//  HMDownloadManager.h
+//  OCToolsClass
+//
+//  Created by 心诚 on 2021/8/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HMDownloadManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OCToolsClass/OCToolsClass/Tools/Download/HMDownloadManager.m
@@ -0,0 +1,12 @@
+//
+//  HMDownloadManager.m
+//  OCToolsClass
+//
+//  Created by 心诚 on 2021/8/24.
+//
+
+#import "HMDownloadManager.h"
+
+@implementation HMDownloadManager
+
+@end
