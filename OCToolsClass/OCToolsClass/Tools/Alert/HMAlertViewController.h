c63b5970f88149dcc65363aa06e208ba59f05361
zhuhc <1647358411@qq.com>
2021-08-31 15:30:34 +0800

    update

--- OCToolsClass/OCToolsClass/Tools/Alert/HMAlertViewController.h
+++ OCToolsClass/OCToolsClass/Tools/Alert/HMAlertViewController.h
@@ -17,10 +17,21 @@ typedef NS_ENUM(NSInteger, HMAlertStyle){
 
 @interface HMAlertViewController : UIViewController
 
+/*!
+ 文本弹框
+ @param title 标题
+ @param message 文本内容
+ @param buttons 按钮文字 目前支持最多2个按钮展示
+ @param callback 回调
+ */
 + (instancetype)alertWithTitle:(nullable NSString *)title
                        message:(nullable NSString *)message
                        buttons:(nullable NSArray<NSString *> *)buttons
                       callback:(void(^)(HMAlertStyle index))callback;
+
+// TODO: - 表格弹框
+
+// TODO: - 输入框弹框
 
 @end
 
